c104dcd5fa6f120f77e9ce60857005c3181a4298
AdminPC <AdminPC@ADMIN>
2025-02-16 14:12:39 +0100

    FFXIV in Foreground FPS improvement

--- Tabnado/Util/KeyDetection.cs
+++ Tabnado/Util/KeyDetection.cs
@@ -1,43 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace Tabnado.Util
 {
-    public class KeyDetection
+    public sealed class KeyDetection
     {
         [DllImport("user32.dll")]
         private static extern short GetAsyncKeyState(int vKey);
-
         [DllImport("user32.dll")]
         private static extern IntPtr GetForegroundWindow();
-
         [DllImport("user32.dll")]
         private static extern int GetWindowThreadProcessId(IntPtr hWnd, out int processId);
 
-        private bool wasKeyPressed = false;
-        private int currentVirtualKey = 0x09;
-        private long lastPressTime = 0;
-        private const int COOLDOWN_MS = 50;
         private const string PROCESS_NAME = "ffxiv_dx11";
+        private const int FOCUS_CHECK_INTERVAL_MS = 200;
+        private const int COOLDOWN_MS = 50;
+        private const int KEY_DOWN_MASK = 0x8000;
 
-        public void SetCurrentKey(int virtualKey)
+        private static readonly long TicksPerMillisecond = Stopwatch.Frequency / 1000;
+        private readonly long focusCheckIntervalTicks;
+        private readonly long cooldownTicks;
+
+        private int currentVirtualKey;
+        private bool wasKeyPressed;
+        private long lastPressTime;
+        private long lastFocusCheck;
+        private bool lastFocusState;
+        private IntPtr cachedWindowHandle;
+        private int cachedProcessId;
+
+        public KeyDetection(int initialVirtualKey = 0x09)
         {
-            currentVirtualKey = virtualKey;
+            currentVirtualKey = initialVirtualKey;
+            focusCheckIntervalTicks = FOCUS_CHECK_INTERVAL_MS * TicksPerMillisecond;
+            cooldownTicks = COOLDOWN_MS * TicksPerMillisecond;
         }
 
-        private bool IsFFXIVFocused()
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetCurrentKey(int virtualKey) => currentVirtualKey = virtualKey;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static long GetCurrentTimeTicks() => Stopwatch.GetTimestamp();
+
+        private bool CheckFocus()
         {
-            IntPtr foregroundWindow = GetForegroundWindow();
-            GetWindowThreadProcessId(foregroundWindow, out int processId);
+            var currentWindow = GetForegroundWindow();
+
+            if (currentWindow == cachedWindowHandle)
+                return lastFocusState;
+
+            cachedWindowHandle = currentWindow;
+            GetWindowThreadProcessId(currentWindow, out cachedProcessId);
 
             try
             {
-                using Process process = Process.GetProcessById(processId);
+                using var process = Process.GetProcessById(cachedProcessId);
                 return process.ProcessName.Equals(PROCESS_NAME, StringComparison.OrdinalIgnoreCase);
             }
             catch
@@ -46,24 +65,44 @@ namespace Tabnado.Util
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool IsFFXIVFocused()
+        {
+            long currentTime = GetCurrentTimeTicks();
+
+            if (currentTime - lastFocusCheck >= focusCheckIntervalTicks)
+            {
+                lastFocusState = CheckFocus();
+                lastFocusCheck = currentTime;
+            }
+
+            return lastFocusState;
+        }
+
         public bool IsKeyPressed()
         {
             if (!IsFFXIVFocused())
                 return false;
 
             short keyState = GetAsyncKeyState(currentVirtualKey);
-            bool isKeyDown = (keyState & 0x8000) != 0;
-            long currentTime = Stopwatch.GetTimestamp() / (Stopwatch.Frequency / 1000);
-            bool isCooldownElapsed = (currentTime - lastPressTime) >= COOLDOWN_MS;
-            bool isPressed = isKeyDown && !wasKeyPressed && isCooldownElapsed;
+            bool isKeyDown = (keyState & KEY_DOWN_MASK) != 0;
 
-            if (isPressed)
+            if (!isKeyDown)
             {
-                lastPressTime = currentTime;
+                wasKeyPressed = false;
+                return false;
             }
 
-            wasKeyPressed = isKeyDown;
-            return isPressed;
+            if (wasKeyPressed)
+                return false;
+
+            long currentTime = GetCurrentTimeTicks();
+            if (currentTime - lastPressTime < cooldownTicks)
+                return false;
+
+            lastPressTime = currentTime;
+            wasKeyPressed = true;
+            return true;
         }
     }
-}
+}                   
